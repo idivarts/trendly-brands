105b4b4eec9199f632f0c897ae7070a2e44d1a10
dev-rathore <rathoredvd3001@gmail.com>
2025-01-29 05:20:45 +0530

    feedbacks

--- styles/settings/Settings.styles.tsx
+++ styles/settings/Settings.styles.tsx
@@ -1,27 +1,13 @@
 import { StyleSheet } from "react-native";
 import { Theme } from "@react-navigation/native";
-import Colors from "@/constants/Colors";
 
 const styles = (theme: Theme) =>
   StyleSheet.create({
     settingsContainer: {
       flex: 1,
-      alignItems: "center",
       justifyContent: "space-between",
-      paddingHorizontal: 20,
-    },
-    settingsRow: {
-      flexDirection: "column",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "100%",
-    },
-    settingsLabel: {
-      fontSize: 18,
-      textAlign: "left",
-      width: "100%",
-      marginVertical: 10,
-      color: Colors(theme).gray100,
+      paddingHorizontal: 16,
+      paddingBottom: 16,
     },
   });
 
