3917c22d2a60e1a83d260affd981378455d6a683
Rahul Sinha <rahul@idiv.in>
2025-04-28 15:47:31 +0530

    Slides temporary fix

--- constants/Slides.ts
+++ constants/Slides.ts
@@ -1,5 +1,11 @@
 export const slides = [
   {
+    key: "connect",
+    title: "Get Started Today",
+    text: "Sign up to start posting collaborations or log in to manage your existing campaigns.",
+    image: require("@/assets/images/design3.png"),
+  },
+  {
     key: "manage",
     title: "Find the Perfect Influencers for Your Brand",
     text: "Connect with influencers who match your brand and drive results.",
@@ -11,10 +17,5 @@ export const slides = [
     text: "We connect you directly with influencers, saving you up to 30% on commissions.",
     image: require("@/assets/images/design2.png"),
   },
-  {
-    key: "connect",
-    title: "Get Started Today",
-    text: "Sign up to start posting collaborations or log in to manage your existing campaigns.",
-    image: require("@/assets/images/design3.png"),
-  },
+
 ];
