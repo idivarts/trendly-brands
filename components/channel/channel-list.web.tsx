5f94886686a265336f5c2ce795f79f7621bb9456
Rahul Sinha <rahul@idiv.in>
2025-05-02 03:46:01 +0530

    Added messenger and its working good

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -1,30 +1,32 @@
-import { View } from "@/components/theme/Themed";
-import { imageUrl } from "@/utils/url";
-import { Linking } from "react-native";
-import EmptyState from "../ui/empty-state";
 
 const ChannelListWeb = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <EmptyState
-        title="Download Mobile App"
-        subtitle="Messaging is only available on mobile app. Download the mobile app today"
-        action={() => {
-          Linking.openURL(
-            "https://apps.apple.com/us/app/trendly-for-brands/id6736949941"
-          );
-        }}
-        actionLabel="Download Mobile App"
-        image={imageUrl(require("@/assets/images/illustration7.png"))}
-      />
-    </View>
+    <iframe
+      src="/assets/messenger/"
+      style={{ width: '100%', height: '100%', border: 'none' }}
+    />
   );
+  // return (
+  //   <View
+  //     style={{
+  //       flex: 1,
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //     }}
+  //   >
+  //     <EmptyState
+  //       title="Download Mobile App"
+  //       subtitle="Messaging is only available on mobile app. Download the mobile app today"
+  //       action={() => {
+  //         Linking.openURL(
+  //           "https://apps.apple.com/us/app/trendly-for-brands/id6736949941"
+  //         );
+  //       }}
+  //       actionLabel="Download Mobile App"
+  //       image={imageUrl(require("@/assets/images/illustration7.png"))}
+  //     />
+  //   </View>
+  // );
 };
 
 export default ChannelListWeb;
