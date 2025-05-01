94384ce610425f670aee90642291bae3886726f6
Rahul Sinha <rahul@idiv.in>
2025-05-02 04:39:01 +0530

    Made minor changes to make the channel list work properly

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -1,32 +1,49 @@
+import Colors from "@/constants/Colors";
+import { useAuthContext } from "@/contexts";
+import { useChatContext } from "@/contexts/chat-context.provider.web";
+import AppLayout from "@/layouts/app-layout";
+import { useTheme } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native-paper";
+import { View } from "../theme/Themed";
+import EmptyMessageState from "./empty-message-state";
 
 const ChannelListWeb = () => {
+  const [loading, setLoading] = useState(true)
+  const [token, setToken] = useState("")
+  const theme = useTheme()
+  const { manager } = useAuthContext()
+  const { connectUser } = useChatContext()
+
+  const fetchToken = async () => {
+    setLoading(true)
+    const token = await connectUser()
+    if (token) {
+      setToken(token)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchToken()
+  }, [])
+
+  if (loading) {
+    return <AppLayout>
+      <View style={{ flex: 1, alignItems: "center", padding: 24 }}>
+        <ActivityIndicator color={Colors(theme).primary} />
+      </View>
+    </AppLayout>
+  }
+  if (!token) {
+    return <EmptyMessageState />
+  }
   return (
     <iframe
-      src="/assets/messenger?skip_name_image_set=false&no_channel_name_filter=false"
+      src={`/assets/messenger?user=${manager?.id}&user_token=${token}&skip_name_image_set=false&no_channel_name_filter=false`}
       style={{ width: '100%', height: '100%', border: 'none' }}
     />
   );
-  // return (
-  //   <View
-  //     style={{
-  //       flex: 1,
-  //       justifyContent: "center",
-  //       alignItems: "center",
-  //     }}
-  //   >
-  //     <EmptyState
-  //       title="Download Mobile App"
-  //       subtitle="Messaging is only available on mobile app. Download the mobile app today"
-  //       action={() => {
-  //         Linking.openURL(
-  //           "https://apps.apple.com/us/app/trendly-for-brands/id6736949941"
-  //         );
-  //       }}
-  //       actionLabel="Download Mobile App"
-  //       image={imageUrl(require("@/assets/images/illustration7.png"))}
-  //     />
-  //   </View>
-  // );
 };
 
 export default ChannelListWeb;
