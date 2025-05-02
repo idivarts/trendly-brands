eb191e4424e95b6ee380d794ca03934d83922f93
Rahul Sinha <rahul@idiv.in>
2025-05-03 00:29:07 +0530

    Removed session to be used as its risky

--- contexts/cloud-messaging.provider.tsx
+++ contexts/cloud-messaging.provider.tsx
@@ -5,6 +5,7 @@ import {
 } from "react";
 
 import { useCloudMessaging } from "@/shared-libs/utils/cloud-messaging";
+import { AuthApp } from "@/shared-libs/utils/firebase/auth";
 import { useAuthContext } from "./auth-context.provider";
 import { streamClient } from "./chat-context.provider";
 
@@ -24,12 +25,11 @@ export const CloudMessagingContextProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   const {
-    session,
-    manager: user,
+    manager,
     updateManager,
   } = useAuthContext();
 
-  const { getToken } = useCloudMessaging(streamClient, session, user, updateManager)
+  const { getToken } = useCloudMessaging(streamClient, AuthApp.currentUser?.uid, manager, updateManager)
 
   return (
     <CloudMessagingContext.Provider
