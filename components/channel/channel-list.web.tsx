95465d130df5d8c61573020026d25b25622a4455
Rahul Sinha <rahul@idiv.in>
2025-05-03 20:03:35 +0530

    removed unneeded re-renders

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -2,21 +2,25 @@ import Colors from "@/constants/Colors";
 import { useAuthContext } from "@/contexts";
 import { useChatContext } from "@/contexts/chat-context.provider.web";
 import AppLayout from "@/layouts/app-layout";
-import { useTheme } from "@react-navigation/native";
+import { IMessengerData } from "@/shared-libs/messenger/interfaces/message-interface";
+import { useIsFocused, useTheme } from "@react-navigation/native";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { ActivityIndicator } from "react-native-paper";
 import { View } from "../theme/Themed";
 import EmptyMessageState from "./empty-message-state";
 
 const ChannelListWeb = () => {
   const [loading, setLoading] = useState(true)
+  const [iFrameLoaded, setIFrameLoaded] = useState(false)
+  const iFramRef = useRef<HTMLIFrameElement>(null)
   const [token, setToken] = useState("")
   const theme = useTheme()
   const router = useRouter()
   const { manager } = useAuthContext()
   const { connectUser } = useChatContext()
-  const params = useLocalSearchParams()
+  const { channelId } = useLocalSearchParams()
+  const isFocused = useIsFocused()
 
   const fetchToken = async () => {
     setLoading(true)
@@ -28,16 +32,25 @@ const ChannelListWeb = () => {
   }
 
   useEffect(() => {
-
     window.addEventListener('message', function (event) {
       console.log("Received event from ifram");
-      if (event.data.type == "open-contract") {
-        const contractId = event.data.data
+      const mData: IMessengerData = event.data;
+      if (mData.type == "open-contract") {
+        const contractId = mData.data
         router.push(`/contract-details/${contractId}`);
-        // window.location.href = event.data.replace('redirect:', '');
       }
     });
   }, [])
+
+  useEffect(() => {
+    if (channelId && (iFrameLoaded || isFocused)) {
+      const mData: IMessengerData = {
+        type: "open-channel",
+        data: channelId
+      }
+      iFramRef.current?.contentWindow?.postMessage(mData)
+    }
+  }, [channelId, iFrameLoaded, isFocused])
 
   useEffect(() => {
     if (manager)
@@ -55,20 +68,17 @@ const ChannelListWeb = () => {
     return <EmptyMessageState />
   }
   const url = `/messenger?user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`
-  console.log("Messenger path", url);
-
+  // console.log("Messenger path", url);
   return (
     <Fragment>
-      {
-        params.channelId ?
-          <iframe
-            src={`/messenger/index.html?channelId=${params.channelId}&user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`}
-            style={{ width: '100%', height: '100%', border: 'none' }}
-          /> : <iframe
-            src={`/messenger/index.html?user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`}
-            style={{ width: '100%', height: '100%', border: 'none' }}
-          />
-      }
+      <iframe
+        ref={iFramRef}
+        src={`/messenger/index.html?user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`}
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        onLoad={() => {
+          setIFrameLoaded(true)
+        }}
+      />
     </Fragment>
   );
 };
