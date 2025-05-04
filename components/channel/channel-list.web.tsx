dcea324c81f7fd9c63e78aaa9cbf64bb7613d9dc
Rahul Sinha <rahul@idiv.in>
2025-05-04 13:03:29 +0530

    Implemented a common web handler

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -2,10 +2,9 @@ import Colors from "@/constants/Colors";
 import { useAuthContext } from "@/contexts";
 import { useChatContext } from "@/contexts/chat-context.provider";
 import AppLayout from "@/layouts/app-layout";
-import { IMessengerData } from "@/shared-libs/messenger/interfaces/message-interface";
-import { useIsFocused, useTheme } from "@react-navigation/native";
-import { useLocalSearchParams, useRouter } from "expo-router";
-import { Fragment, useEffect, useRef, useState } from "react";
+import WebMessageWrapper from "@/shared-libs/contexts/web-message-wrapper";
+import { useTheme } from "@react-navigation/native";
+import { useEffect, useRef, useState } from "react";
 import { ActivityIndicator } from "react-native-paper";
 import { View } from "../theme/Themed";
 import EmptyMessageState from "./empty-message-state";
@@ -16,11 +15,8 @@ const ChannelListWeb = () => {
   const iFramRef = useRef<HTMLIFrameElement>(null)
   const [token, setToken] = useState("")
   const theme = useTheme()
-  const router = useRouter()
   const { manager } = useAuthContext()
   const { connectUser } = useChatContext()
-  const { channelId } = useLocalSearchParams()
-  const isFocused = useIsFocused()
 
   const fetchToken = async () => {
     setLoading(true)
@@ -31,26 +27,6 @@ const ChannelListWeb = () => {
     setLoading(false)
   }
 
-  useEffect(() => {
-    window.addEventListener('message', function (event) {
-      console.log("Received event from ifram");
-      const mData: IMessengerData = event.data;
-      if (mData.type == "open-contract") {
-        const contractId = mData.data
-        router.push(`/contract-details/${contractId}`);
-      }
-    });
-  }, [])
-
-  useEffect(() => {
-    if (channelId && (iFrameLoaded || isFocused)) {
-      const mData: IMessengerData = {
-        type: "open-channel",
-        data: channelId
-      }
-      iFramRef.current?.contentWindow?.postMessage(mData)
-    }
-  }, [channelId, iFrameLoaded, isFocused])
 
   useEffect(() => {
     if (manager)
@@ -67,10 +43,8 @@ const ChannelListWeb = () => {
   if (!token) {
     return <EmptyMessageState />
   }
-  const url = `/messenger?user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`
-  // console.log("Messenger path", url);
   return (
-    <Fragment>
+    <WebMessageWrapper iFrameLoaded={iFrameLoaded} iFrameRef={iFramRef}>
       <iframe
         ref={iFramRef}
         src={`/messenger/index.html?user=${manager?.id}&user_token=${token}&target_origin=${window.location.origin}&skip_name_image_set=false&no_channel_name_filter=false`}
@@ -79,7 +53,7 @@ const ChannelListWeb = () => {
           setIFrameLoaded(true)
         }}
       />
-    </Fragment>
+    </WebMessageWrapper>
   );
 };
 
