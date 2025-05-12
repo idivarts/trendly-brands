0d90e75e98f142cbd934121dfb3157a222fe5f73
Rahul Sinha <rahul@idiv.in>
2025-05-12 16:10:48 +0530

    Replaced application and invitation with the Influencer card

--- components/card/collaboration-details/application-card.tsx
+++ components/card/collaboration-details/application-card.tsx
@@ -1,22 +1,12 @@
-import { Card } from "@/components/ui/card/secondary";
-import { CardDescription } from "@/components/ui/card/secondary/card-description";
 import { CardFooter } from "@/components/ui/card/secondary/card-footer";
-import { CardHeader } from "@/components/ui/card/secondary/card-header";
 import Colors from "@/constants/Colors";
-import { MAX_WIDTH_WEB } from "@/constants/Container";
-import { Attachment } from "@/shared-libs/firestore/trendly-pro/constants/attachment";
-import Carousel from "@/shared-uis/components/carousel/carousel";
-import { InfluencerMetrics } from "@/shared-uis/components/influencers/influencer-metrics";
+import InfluencerCard from "@/shared-uis/components/InfluencerCard";
 import { InfluencerApplication } from "@/types/Collaboration";
-import { processRawAttachment } from "@/utils/attachments";
 import { convertToKUnits } from "@/utils/conversion";
-import { formatTimeToNow } from "@/utils/date";
-import { truncateText } from "@/utils/text";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useTheme } from "@react-navigation/native";
-import React from "react";
-import { Dimensions, Platform, Pressable, View } from "react-native";
+import React, { useState } from "react";
 import Button from "../../ui/button";
 
 interface ApplicationCardProps {
@@ -33,65 +23,51 @@ export const ApplicationCard: React.FC<ApplicationCardProps> = ({
   profileModalAction,
 }) => {
   const theme = useTheme();
-
+  const [influencer, setInfluencer] = useState(data.influencer)
   return (
-    <Card>
-      <CardHeader
-        avatar={data.influencer.profileImage || ""}
-        handle={data.influencer.socials?.[0]}
-        isVerified={data.influencer.isVerified}
-        leftAction={profileModalAction}
-        name={data.influencer.name}
-        rightAction={bottomSheetAction}
-        timestamp={formatTimeToNow(data.application.timeStamp)}
-      />
-      <Carousel
-        containerHeight={
-          data.application?.attachments?.length === 1
-            ? Platform.OS === "web"
-              ? 560
-              : 288
-            : undefined
-        }
-        data={data.application.attachments.map((attachment: Attachment) =>
-          processRawAttachment(attachment)
-        )}
-        theme={theme}
-        carouselWidth={
-          Platform.OS === "web" ? MAX_WIDTH_WEB : Dimensions.get("window").width
-        }
-      />
-      <Pressable onPress={profileModalAction}>
-        <View style={{ paddingHorizontal: 16 }}>
-          <InfluencerMetrics user={data.influencer} action={<Button
-            mode="outlined"
-            size="small"
-            onPress={() => {
-              if (data.application.status === "pending") {
-                acceptApplication();
-              }
+    <>
+      <InfluencerCard
+        influencer={{
+          ...influencer, profile: {
+            ...influencer.profile,
+            content: {
+              ...influencer.profile?.content,
+              about: data.application.message
+            }
+          }
+        }}
+        openProfile={profileModalAction}
+        ToggleModal={bottomSheetAction}
+        type="application"
+        customAttachments={data.application.attachments}
+        cardActionNode={<Button
+          mode="outlined"
+          size="small"
+          onPress={() => {
+            if (data.application.status === "pending") {
+              acceptApplication();
+            }
+          }}
+        >
+          <FontAwesomeIcon
+            color={Colors(theme).primary}
+            icon={faCheck}
+            size={12}
+            style={{
+              marginRight: 6,
+              marginTop: -2,
             }}
-          >
-            <FontAwesomeIcon
-              color={Colors(theme).primary}
-              icon={faCheck}
-              size={12}
-              style={{
-                marginRight: 6,
-                marginTop: -2,
-              }}
-            />
-            {data.application.status === "pending" ? "Accept" : "Accepted"}
-          </Button>} />
-        </View>
-        <CardDescription text={truncateText(data.application.message, 160)} />
-        <CardFooter
+          />
+          {data.application.status === "pending" ? "Accept" : "Accepted"}
+        </Button>}
+        footerNode={<CardFooter
           quote={convertToKUnits(Number(data.application.quotation)) as string}
           timeline={new Date(data.application.timeline).toLocaleDateString(
             "en-US"
           )}
-        />
-      </Pressable>
-    </Card>
+        />}
+      />
+    </>
+
   );
 };
--- components/card/collaboration-details/invitation-card.tsx
+++ components/card/collaboration-details/invitation-card.tsx
@@ -1,21 +1,12 @@
 import Colors from "@/constants/Colors";
-import { MAX_WIDTH_WEB } from "@/constants/Container";
 import useInvitation from "@/hooks/use-invitation";
-import Carousel from "@/shared-uis/components/carousel/carousel";
-import { InfluencerMetrics } from "@/shared-uis/components/influencers/influencer-metrics";
-import { View } from "@/shared-uis/components/theme/Themed";
+import InfluencerCard from "@/shared-uis/components/InfluencerCard";
 import { User } from "@/types/User";
-import { processRawAttachment } from "@/utils/attachments";
-import { truncateText } from "@/utils/text";
 import { faCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useTheme } from "@react-navigation/native";
 import React from "react";
-import { Dimensions, Platform, Pressable } from "react-native";
 import Button from "../../ui/button";
-import { Card } from "../../ui/card/secondary";
-import { CardDescription } from "../../ui/card/secondary/card-description";
-import { CardHeader } from "../../ui/card/secondary/card-header";
 
 interface InvitationCardProps {
   bottomSheetAction?: () => void;
@@ -39,60 +30,33 @@ const InvitationCard: React.FC<InvitationCardProps> = ({
   });
 
   return (
-    <Card>
-      <CardHeader
-        avatar={data?.profileImage || ""}
-        handle={data.socials?.[0]}
-        isVerified={data.isVerified}
-        leftAction={profileModalAction}
-        name={data.name}
-        rightAction={bottomSheetAction}
-      />
-      <Carousel
-        containerHeight={
-          data.profile?.attachments?.length === 1
-            ? Platform.OS === "web"
-              ? 560
-              : 288
-            : undefined
-        }
-        data={
-          data.profile?.attachments?.map((attachment) =>
-            processRawAttachment(attachment)
-          ) || []
-        }
-        theme={theme}
-        carouselWidth={
-          Platform.OS === "web" ? MAX_WIDTH_WEB : Dimensions.get("window").width
-        }
-      />
-      <Pressable onPress={profileModalAction}>
-        <View style={{ paddingHorizontal: 16 }}>
-          <InfluencerMetrics user={data} action={<Button
-            mode="outlined"
-            onPress={() => {
-              if (!isAlreadyInvited) {
-                inviteInfluencer();
-              }
+    <>
+      <InfluencerCard
+        influencer={data}
+        type="invitation"
+        ToggleModal={bottomSheetAction}
+        cardActionNode={<Button
+          mode="outlined"
+          onPress={() => {
+            if (!isAlreadyInvited) {
+              inviteInfluencer();
+            }
+          }}
+        >
+          <FontAwesomeIcon
+            color={Colors(theme).primary}
+            icon={isAlreadyInvited ? faCheck : faPlus}
+            size={12}
+            style={{
+              marginRight: 6,
+              marginTop: -1,
             }}
-          >
-            <FontAwesomeIcon
-              color={Colors(theme).primary}
-              icon={isAlreadyInvited ? faCheck : faPlus}
-              size={12}
-              style={{
-                marginRight: 6,
-                marginTop: -1,
-              }}
-            />
-            {isAlreadyInvited ? "Already Invited" : "Invite"}
-          </Button>} />
-        </View>
-        <CardDescription
-          text={truncateText(data.profile?.content?.about || "", 160)}
-        />
-      </Pressable>
-    </Card>
+          />
+          {isAlreadyInvited ? "Already Invited" : "Invite"}
+        </Button>}
+        openProfile={profileModalAction}
+      />
+    </>
   );
 };
 
