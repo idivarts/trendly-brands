5e7f2831f6e05a80fc63e0a389c26d64b3be08d3
Rahul Sinha <rahul@idiv.in>
2025-06-10 00:23:30 +0530

    Added condition to only add brands that have accepted invited

--- contexts/brand-context.provider.tsx
+++ contexts/brand-context.provider.tsx
@@ -1,4 +1,4 @@
-import { IBrands } from "@/shared-libs/firestore/trendly-pro/models/brands";
+import { IBrands, IBrandsMembers } from "@/shared-libs/firestore/trendly-pro/models/brands";
 import { Console } from "@/shared-libs/utils/console";
 import { FirestoreDB } from "@/shared-libs/utils/firebase/firestore";
 import { PersistentStorage } from "@/shared-libs/utils/persistent-storage";
@@ -56,7 +56,7 @@ export const BrandContextProvider: React.FC<PropsWithChildren> = ({
     const membersQuery = query(
       membersCollection,
       where("managerId", "==", manager.id),
-      where("status", "not-in", [0, 2])
+      // where("status", "not-in", [0, 2])
     );
     Console.log("Brand ID from member Query:", manager.id);
 
@@ -71,6 +71,11 @@ export const BrandContextProvider: React.FC<PropsWithChildren> = ({
 
       const brandIds = new Set<string>();
       membersSnapshot.docs.forEach((doc) => {
+        const member = doc.data() as IBrandsMembers
+        if (member.status === 0 || member.status > 1) {
+          return
+        }
+
         const brandId = doc.ref.parent.parent?.id; // Get the brand ID from the member's document reference
         Console.log("Brand ID from member:", brandId);
         if (brandId) {
