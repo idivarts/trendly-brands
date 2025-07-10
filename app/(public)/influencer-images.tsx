2c79570c0069e5961ec2a565a8a02e3b26fbbf46
Rahul Sinha <rahul@idiv.in>
2025-07-11 01:04:31 +0530

    Added influencer images listing

---
+++ app/(public)/influencer-images.tsx
@@ -0,0 +1,76 @@
+import AppLayout from '@/layouts/app-layout';
+import { IUsers } from '@/shared-libs/firestore/trendly-pro/models/users';
+import { FirestoreDB } from '@/shared-libs/utils/firebase/firestore';
+import { FlashList } from '@shopify/flash-list';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { Image, Platform, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { ActivityIndicator } from 'react-native-paper';
+
+interface InfluencerInterface { id: string, images: { imageUrl: string, id: string }[], totalImages: number, user?: IUsers }
+const InfluencerList = () => {
+
+    const [influencerList, setInfluencerList] = useState<InfluencerInterface[]>([])
+    const [loading, setLoading] = useState(true)
+    // const { push } = useMyNavigation()
+
+    const getInfluencer = async () => {
+        try {
+            setLoading(true)
+            const userImagesRef = collection(FirestoreDB, "userImages");
+            const snapshot = await getDocs(userImagesRef);
+            const list: (InfluencerInterface | undefined)[] = await Promise.all(snapshot.docs.map(async (docData) => {
+                if (docData.id == "config")
+                    return undefined
+                const data = docData.data() as InfluencerInterface;
+                // const userDoc = await getDoc(doc(collection(FirestoreDB, "users"), docData.id));
+                return {
+                    ...data,
+                    id: docData.id,
+                    // user: userDoc.data() as IUsers,
+                };
+            }));
+            setInfluencerList(list.filter(l => l) as InfluencerInterface[]);
+        } finally {
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        getInfluencer()
+    }, [])
+
+    if (Platform.OS != "web" || window.location.hostname != "localhost")
+        return null
+    if (loading) {
+        <ActivityIndicator />
+    }
+    return (
+        <AppLayout>
+            <Text style={{ fontSize: 22, fontWeight: '700', paddingHorizontal: 16, marginBottom: 8 }}>Influencer List - {influencerList.length}</Text>
+            <ScrollView style={{ flex: 1, height: "100%" }}>
+                <FlashList
+                    data={influencerList}
+                    renderItem={({ item, index }) => {
+                        return <View>
+                            <Text onPress={() => window.open(`/influencer/${item.id}`, "_blank")}>Influencer Id: {item.id}</Text>
+                            <FlashList
+                                data={item.images}
+                                renderItem={({ item }) => {
+                                    return <Image
+                                        source={{ uri: item.imageUrl }}
+                                        style={{ width: 150, height: 300, borderRadius: 8, marginRight: 16, borderWidth: 1 }}
+                                    />
+                                }}
+                                horizontal={true} />
+                        </View>
+                    }}
+                />
+
+            </ScrollView>
+        </AppLayout >
+
+    )
+}
+
+export default InfluencerList
