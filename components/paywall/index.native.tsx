8906f1f21b5419b19701bac189826aa136f3a27b
Rahul Sinha <rahul@idiv.in>
2025-07-30 21:47:38 +0530

    Added paywall

---
+++ components/paywall/index.native.tsx
@@ -0,0 +1,10 @@
+import { View } from '@/shared-uis/components/theme/Themed'
+import React from 'react'
+
+const PayWallComponent = () => {
+    return (
+        <View></View>
+    )
+}
+
+export default PayWallComponent
---
+++ components/paywall/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from '@/shared-uis/components/theme/Themed'
+import React from 'react'
+
+const PayWallComponent = () => {
+    return (
+        <View>
+            <Text>PayWall here</Text>
+        </View>
+    )
+}
+
+export default PayWallComponent
