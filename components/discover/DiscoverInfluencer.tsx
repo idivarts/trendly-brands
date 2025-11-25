2c3b3221ae768ce261034616053100bf71a2c793
Rahul Sinha <rahul@idiv.in>
2025-11-25 14:39:54 +0530

    Done changes on inflrencer Item object

--- components/discover/DiscoverInfluencer.tsx
+++ components/discover/DiscoverInfluencer.tsx
@@ -30,17 +30,50 @@ import InfluencerCard from "../explore-influencers/InfluencerCard";
 import DiscoverPlaceholder from "./DiscoverAdPlaceholder";
 import { InfluencerStatsModal } from "./InfluencerStatModal";
 
+
+// type SocialsBreif struct {
+// 	ID       string `db:"id" bigquery:"id" json:"id" firestore:"id"`
+// 	Name     string `db:"name" bigquery:"name" json:"name" firestore:"name"`
+// 	Username string `db:"username" bigquery:"username" json:"username" firestore:"username"`
+
+// 	ProfilePic      string  `db:"profile_pic" bigquery:"profile_pic" json:"profile_pic" firestore:"profile_pic"`
+// 	FollowerCount   int64   `db:"follower_count" bigquery:"follower_count" json:"follower_count" firestore:"follower_count"`
+// 	ViewsCount      int64   `db:"views_count" bigquery:"views_count" json:"views_count" firestore:"views_count"`                      //views
+// 	EnagamentsCount int64   `db:"engagement_count" bigquery:"engagements_count" json:"engagement_count" firestore:"engagement_count"` //engagement
+// 	EngagementRate  float32 `db:"engagement_rate" bigquery:"engagement_rate" json:"engagement_rate" firestore:"engagement_rate"`
+
+// 	SocialType string `db:"social_type" bigquery:"social_type" json:"social_type" firestore:"social_type"`
+
+// 	Location string `db:"location" bigquery:"location" json:"location" firestore:"location"`
+
+// 	Bio string `db:"bio" bigquery:"bio" json:"bio" firestore:"bio"`
+
+// 	ProfileVerified bool `db:"profile_verified" bigquery:"profile_verified" json:"profile_verified" firestore:"profile_verified"`
+
+// 	CreationTime   int64 `db:"creation_time" bigquery:"creation_time" json:"creation_time" firestore:"creation_time"`
+// 	LastUpdateTime int64 `db:"last_update_time" bigquery:"last_update_time" json:"last_update_time" firestore:"last_update_time"`
+// }
 // Types
 export interface InfluencerItem {
-  userId: string;
-  fullname: string;
+  id: string;
+  name: string;
   username: string;
-  url: string;
-  picture: string;
-  followers: number;
-  views?: number;
-  engagements: number;
-  engagementRate: number;
+  profile_pic: string;
+  follower_count: number;
+  views_count?: number;
+  engagement_count: number;
+  engagement_rate: number;
+  social_type?: string;
+  location?: string;
+  bio?: string;
+  profile_verified?: boolean;
+  creation_time?: number;
+  last_update_time?: number;
+
+  isDiscover?: boolean;
+
+  // For invitation card
+  invitedAt?: number; // timestamp in milliseconds
   status?: string;
 }
 
@@ -189,7 +222,7 @@ const DiscoverInfluencer: React.FC<DiscoverInfluencerProps> = ({
     if (
       (selectedBrand?.credits?.discovery || 0) <= 0 &&
       data &&
-      !selectedBrand?.discoveredInfluencers?.includes(data.userId)
+      !selectedBrand?.discoveredInfluencers?.includes(data.id)
     ) {
       openModal({
         title: "No Discovery Credit",
@@ -271,8 +304,8 @@ const DiscoverInfluencer: React.FC<DiscoverInfluencerProps> = ({
             isCollapsed={isCollapsed}
             onPress={() => setStatsItem(item)}
             openModal={openModal}
-            isSelected={selectedIds.includes(item.userId)}
-            onToggleSelect={() => toggleSelect(item.userId)}
+            isSelected={selectedIds.includes(item.id)}
+            onToggleSelect={() => toggleSelect(item.id)}
             isStatusCard={isStatusCard}
           />
         </View>
@@ -281,7 +314,7 @@ const DiscoverInfluencer: React.FC<DiscoverInfluencerProps> = ({
     [isCollapsed, openModal, setStatsItem]
   );
 
-  const keyExtractor = useCallback((i: InfluencerItem) => i.userId, []);
+  const keyExtractor = useCallback((i: InfluencerItem) => i.id, []);
 
   const getItemLayout = useCallback(
     (_: InfluencerItem[] | null | undefined, index: number) => ({
@@ -595,10 +628,10 @@ const DiscoverInfluencer: React.FC<DiscoverInfluencerProps> = ({
               </Text>
 
               {/* Invite Button */}
-              <View style={{ top: 0, borderRadius:50 }}>
+              <View style={{ top: 0, borderRadius: 50 }}>
                 <InviteToCampaignButton
                   label="Invite Now"
-                  openModal={() => {}}
+                  openModal={() => { }}
                 />
               </View>
 
