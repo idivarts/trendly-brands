95230b5659e3e236f8a68879e5f36838485fb05b
Rahul Sinha <rahul@idiv.in>
2026-01-28 21:22:55 +0530

    Added is-ignore

--- components/discover/Discover.tsx
+++ components/discover/Discover.tsx
@@ -50,10 +50,10 @@ const DiscoverComponent = ({
     const { selectedBrand } = useBrandContext();
     const [rightPanel, setRightPanel] = useState(false);
     const [showFilters, setShowFilters] = useState(false);
-    const discoverCommunication =
-        useRef<(action: DiscoverCommunication) => any>();
-    const pageSortCommunication =
-        useRef<(action: PageSortCommunication) => any>();
+    // @ts-ignore
+    const discoverCommunication = useRef<(action: DiscoverCommunication) => any>();
+    // @ts-ignore
+    const pageSortCommunication = useRef<(action: PageSortCommunication) => any>();
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [storedFilters, setStoredFilters] = useState<IAdvanceFilters | null>(
         null
