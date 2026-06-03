d7af6686ae2a152bfe9432e99521a982630efd16
Rahul Sinha <rahul@idiv.in>
2026-06-03 23:20:19 +0530

    Fixed messages issue

--- components/channel/channel-list.web.tsx
+++ components/channel/channel-list.web.tsx
@@ -1,12 +1,13 @@
-import Colors from "@/shared-uis/constants/Colors";
 import { useAuthContext } from "@/contexts";
 import { useChatContext } from "@/contexts/chat-context.provider";
 import AppLayout from "@/layouts/app-layout";
 import WebMessageWrapper from "@/shared-libs/contexts/web-message-wrapper";
+import Colors from "@/shared-uis/constants/Colors";
 import { useTheme } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native-paper";
 import { View } from "../theme/Themed";
+import PageHeader from "../ui/page-header";
 import EmptyMessageState from "./empty-message-state";
 
 const ChannelListWeb = () => {
@@ -31,23 +32,27 @@ const ChannelListWeb = () => {
             fetchToken()
     }, [manager])
 
+    let content = null;
     if (loading) {
-        return <AppLayout>
+        content = <AppLayout>
             <View style={{ flex: 1, alignItems: "center", padding: 24 }}>
                 <ActivityIndicator color={Colors(theme).primary} />
             </View>
         </AppLayout>
     }
     if (!token) {
-        return <EmptyMessageState />
-    }
-    return (
-        <WebMessageWrapper
+        content = <EmptyMessageState />
+    } else {
+        content = <WebMessageWrapper
             isInfluencer={false}
             influencerManagerid={manager?.id || ""}
             streamToken={token}
         />
-    );
+    }
+    return <AppLayout>
+        <PageHeader title="Messages" />
+        {content}
+    </AppLayout>;
 };
 
 export default ChannelListWeb;
